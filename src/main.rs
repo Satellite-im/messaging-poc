c71ecf86461c5700b6f04f0daca7ff3fccfd3c4b
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-09-13 09:58:36 -0400

    saving progress. doesn't work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 //use crate::{compose::Compose, sidebar::Sidebar};
 
+use futures::StreamExt;
 use std::collections::VecDeque;
 
 use dioxus::prelude::*;
@@ -28,6 +29,7 @@ let observer3 = new IntersectionObserver( (entries, observer) => {
                 dioxus.send("{\"Bottom\":null}");
             } else if (!entry.target.previousElementSibling) {
                 dioxus.send("{\"Top\":null}");
+                //observer.disconnect();
             }
         } else {
             dioxus.send("{\"Remove\":" + entry.target.id + "}");
@@ -53,11 +55,11 @@ fn app(cx: Scope) -> Element {
     let css = include_str!(".styles.css");
     let v: Vec<u32> = (0..100).collect();
     let eval_provider = dioxus_html::prelude::use_eval(cx);
-    println!("rendering app");
 
     let to_take = use_state(cx, || 20);
-    let should_scroll_to_top = use_ref(cx, || false);
-    let eval_id = use_state(cx, || 0);
+    let scroll_to: &UseRef<Option<u32>> = use_ref(cx, || None);
+    let effect_id = use_state(cx, || 0);
+
     let to_render: Vec<_> = v
         .iter()
         .rev()
@@ -65,85 +67,100 @@ fn app(cx: Scope) -> Element {
         .rev()
         .cloned()
         .collect();
-
+    println!("rendering app. to_take is {}", to_take.current());
     let msg_list = use_ref(cx, SortedList::new);
 
-    let scroll_script = if *should_scroll_to_top.read() && msg_list.read().get_min().is_some() {
-        let id = msg_list.read().get_min().unwrap_or_default();
-        println!("scrolling to id {id}");
-        format!("document.querySelectorAll(\"#{id}\").forEach((elem) => elem.scrollIntoView());")
-    } else {
-        // scroll to the bottom
-        "window.scrollTo(0, document.body.scrollHeight);".into()
+    let scroll_script = match scroll_to.read().as_ref() {
+        Some(id) => {
+            println!("scrolling to id {id}");
+            let s = r##"
+var message = document.getElementById("$MESSAGE_ID");
+message.scrollIntoView({ behavior: 'smooth', block: 'start' });
+"##;
+            s.replace("$MESSAGE_ID", &format!("{id}"))
+        }
+        None => "window.scrollTo(0, document.body.scrollHeight);".into(),
     };
 
-    use_future(cx, eval_id.get(), |_| {
-        to_owned![
-            eval_provider,
-            to_take,
-            msg_list,
-            should_scroll_to_top,
-            eval_id
-        ];
-        async move {
-            println!("restarting use_future");
-            let eval = match eval_provider(OBSERVER_SCRIPT) {
-                Ok(r) => r,
-                Err(e) => {
-                    eprintln!("use eval failed: {:?}", e);
-                    return;
-                }
-            };
+    if scroll_to.read().is_some() {
+        scroll_to.write_silent().take();
+    }
 
-            loop {
-                match eval.recv().await {
-                    Ok(msg) => {
-                        //println!("got this from js: {msg}");
-                        if let Some(s) = msg.as_str() {
-                            match serde_json::from_str::<JsMsg>(s) {
-                                Ok(msg) => match msg {
-                                    JsMsg::Add(x) => {
-                                        msg_list.write_silent().insert(x);
-                                        println!(
-                                            "new max: {:?}; new min: {:?}",
-                                            msg_list.read().get_max(),
-                                            msg_list.read().get_min()
-                                        );
-                                    }
-                                    JsMsg::Remove(x) => {
-                                        msg_list.write_silent().remove(x);
-                                        println!(
-                                            "new max: {:?}; new min: {:?}",
-                                            msg_list.read().get_max(),
-                                            msg_list.read().get_min()
-                                        );
-                                    }
-                                    JsMsg::Top => {
-                                        println!("top reached");
-                                        let y = *to_take.current();
-                                        if y < v.len() {
-                                            let x = std::cmp::min(y + 20, v.len());
-                                            *should_scroll_to_top.write_silent() = true;
-                                            to_take.set(x);
-                                            // eval_id.with_mut(|x| *x += 1);
+    let ch = use_coroutine(cx, move |mut rx: UnboundedReceiver<()>| {
+        to_owned![eval_provider, to_take, msg_list, scroll_to, v, effect_id];
+        async move {
+            println!("starting use_future");
+            while rx.next().await.is_some() {
+                println!("use_future loop");
+                let eval = match eval_provider(OBSERVER_SCRIPT) {
+                    Ok(r) => r,
+                    Err(e) => {
+                        eprintln!("use eval failed: {:?}", e);
+                        return;
+                    }
+                };
+
+                'HANDLE_EVAL: loop {
+                    match eval.recv().await {
+                        Ok(msg) => {
+                            //println!("got this from js: {msg}");
+                            if let Some(s) = msg.as_str() {
+                                match serde_json::from_str::<JsMsg>(s) {
+                                    Ok(msg) => match msg {
+                                        JsMsg::Add(x) => {
+                                            msg_list.write_silent().insert(x);
+                                            println!(
+                                                "new max: {:?}; new min: {:?}",
+                                                msg_list.read().get_max(),
+                                                msg_list.read().get_min()
+                                            );
                                         }
+                                        JsMsg::Remove(x) => {
+                                            msg_list.write_silent().remove(x);
+                                            println!(
+                                                "new max: {:?}; new min: {:?}",
+                                                msg_list.read().get_max(),
+                                                msg_list.read().get_min()
+                                            );
+                                        }
+                                        JsMsg::Top => {
+                                            println!("top reached");
+                                            let y = *to_take.current();
+                                            if y < v.len() {
+                                                let x = std::cmp::min(y + 20, v.len());
+                                                *scroll_to.write() =
+                                                    msg_list.read().get_min().map(|x| x as _);
+                                                to_take.set(x);
+                                                effect_id.with_mut(|x| *x = (*x + 1) % 3);
+                                                //let _ = eval.join().await;
+                                                break 'HANDLE_EVAL;
+                                            }
+                                        }
+                                        JsMsg::Bottom => {
+                                            println!("bottom reached");
+                                        }
+                                    },
+                                    Err(e) => {
+                                        eprintln!("failed to deserialize message: {}: {}", s, e);
                                     }
-                                    JsMsg::Bottom => {
-                                        println!("bottom reached");
-                                    }
-                                },
-                                Err(e) => {
-                                    eprintln!("failed to deserialize message: {}: {}", s, e);
                                 }
                             }
                         }
-                    }
-                    Err(e) => {
-                        println!("eval failed: {e:?}");
-                        break;
-                    }
-                };
+                        Err(e) => {
+                            println!("eval failed: {e:?}");
+                            break;
+                        }
+                    };
+                }
             }
+        }
+    });
+
+    use_effect(cx, (&*effect_id.get()), move |_| {
+        to_owned![ch];
+        async move {
+            println!("use_effect");
+            ch.send(());
         }
     });
 
@@ -168,6 +185,12 @@ fn app(cx: Scope) -> Element {
                     println!("div scrolled");
                 },
                 ul {
+                    onmounted: move |_| {
+                        // todo: handle scrolling here
+                        // only good for calling something the first time the element renders
+                        println!("list is mounted");
+                        ch.send(());
+                    },
                     id: "compose-list",
                     onscroll: move |_evt| {
                         // doesn't fire
