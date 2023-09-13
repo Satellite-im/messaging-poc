af8973e049f71e5d1757d5ce79b8a6b71d5ccd30
Stuart Woodbury <stuartwoodbury@protonmail.ch>
2023-09-13 00:18:29 -0400

    better way to detect when the top/bottom is reached

--- src/main.rs
+++ src/main.rs
@@ -20,37 +20,15 @@ enum JsMsg {
 // https://github.com/DioxusLabs/dioxus/pull/1080
 
 const OBSERVER_SCRIPT: &str = r###"
-
-let options = {
-    root: null, //document.querySelector("#compose-list"),
-    rootMargin: "0px",
-    threshold: 0.75,
-};
-let observer = new IntersectionObserver( (entries, observer) => {
-    if (entries[0].isIntersecting) {
-        dioxus.send("{\"Top\":null}");
-   }
-}, options);
-
-observer.observe(document.querySelector("li:first-child"));
-
-let options2 = {
-    root: null, // document.querySelector("#compose-list"),
-    rootMargin: "0px",
-    threshold: 0.75,
-};
-let observer2 = new IntersectionObserver( (entries, observer) => {
-    if (entries[0].isIntersecting) {
-         dioxus.send("{\"Bottom\":null}");
-    }
-}, options);
-
-observer2.observe(document.querySelector("li:last-child"));
-
 let observer3 = new IntersectionObserver( (entries, observer) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             dioxus.send("{\"Add\":" + entry.target.id + "}");
+            if (!entry.target.nextElementSibling) {
+                dioxus.send("{\"Bottom\":null}");
+            } else if (!entry.target.previousElementSibling) {
+                dioxus.send("{\"Top\":null}");
+            }
         } else {
             dioxus.send("{\"Remove\":" + entry.target.id + "}");
         }
@@ -63,16 +41,8 @@ let observer3 = new IntersectionObserver( (entries, observer) => {
 const elements = document.querySelectorAll("#compose-list > li");
 elements.forEach( (element) => {
     let id = "#" + element.id;
-    // dioxus.send("observing " + id);
     observer3.observe(element);
 });
-
-window.scrollTo(0, document.body.scrollHeight);
-
-// const elem = document.getElementById("compose");
-// const rect = elem.getBoundingClientRect();
-// dioxus.send("rect height: " + rect["height"]);
-// dioxus.send("rect width: " + rect["width"]);
 "###;
 
 fn main() {
@@ -87,6 +57,7 @@ fn app(cx: Scope) -> Element {
 
     let to_take = use_state(cx, || 20);
     let should_scroll_to_top = use_ref(cx, || false);
+    let eval_id = use_state(cx, || 0);
     let to_render: Vec<_> = v
         .iter()
         .rev()
@@ -97,18 +68,25 @@ fn app(cx: Scope) -> Element {
 
     let msg_list = use_ref(cx, SortedList::new);
 
-    if *should_scroll_to_top.read() {
-        *should_scroll_to_top.write_silent() = false;
-        if let Some(id) = msg_list.read().get_min() {
-            println!("scrolling to top: {}", id);
-            let scroll_script = format!("const elements = document.querySelectorAll(\"#{id}\"); elements.forEach((elem) => elem.scrollIntoView(true));");
-            _ = eval_provider(&scroll_script);
-        }
-    }
+    let scroll_script = if *should_scroll_to_top.read() && msg_list.read().get_min().is_some() {
+        let id = msg_list.read().get_min().unwrap_or_default();
+        println!("scrolling to id {id}");
+        format!("document.querySelectorAll(\"#{id}\").forEach((elem) => elem.scrollIntoView());")
+    } else {
+        // scroll to the bottom
+        "window.scrollTo(0, document.body.scrollHeight);".into()
+    };
 
-    use_future(cx, (), |_| {
-        to_owned![eval_provider, to_take, msg_list, should_scroll_to_top];
+    use_future(cx, eval_id.get(), |_| {
+        to_owned![
+            eval_provider,
+            to_take,
+            msg_list,
+            should_scroll_to_top,
+            eval_id
+        ];
         async move {
+            println!("restarting use_future");
             let eval = match eval_provider(OBSERVER_SCRIPT) {
                 Ok(r) => r,
                 Err(e) => {
@@ -147,6 +125,7 @@ fn app(cx: Scope) -> Element {
                                             let x = std::cmp::min(y + 20, v.len());
                                             *should_scroll_to_top.write_silent() = true;
                                             to_take.set(x);
+                                            // eval_id.with_mut(|x| *x += 1);
                                         }
                                     }
                                     JsMsg::Bottom => {
@@ -200,6 +179,7 @@ fn app(cx: Scope) -> Element {
                     }))
                 }
             },
+            script { scroll_script },
         }
     }
 }
